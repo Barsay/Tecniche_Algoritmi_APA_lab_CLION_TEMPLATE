e860904cf04f3d6c3ca8d6ecc78e59104d9adb36
Thomas <thomas.gallian@outlook.com>
2022-10-20 21:23:53 +0200

    chore: added file open/read and fixes

--- EZZ/main.c
+++ EZZ/main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+#define INPUTFILE_PATH "./in.txt"
 
 int main(int argc, char ** argv){
+    char buf[100];
+    FILE *fp = fopen(INPUTFILE_PATH, "r");
+    fgets(buf, 100,fp);
+
     printf("Hello Template!\n");
+    printf("%s", buf);
+
     return 0;
 }
